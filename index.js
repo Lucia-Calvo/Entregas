aec4e975ddb47f91bd07748df85b72afd5d90c8c
Lucia Calvo <lucalvo01@hotmail.com>
2023-05-23 19:47:11 -0300

    segunda entrega

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ class ProductManager {
         };
     this.products.push(product);
     return product;
+    return id;
     }
 
     getProductById(id) {
@@ -39,30 +40,87 @@ class ProductManager {
 
         return product;
     }
+
+    updateProduct(id, field, value) {
+        const product = this.getProductById(id);
+        if (field !== 'id') {
+            product[field] = value;
+        }
+        return product;
+    }
+        
+    deleteProduct(id) {
+        const index = this.products.findIndex(product => product.id === id);
+        if (index === -1) {
+            throw new Error('Producto no encontrado');
+        }
+        this.products.splice(index, 1);
+    }
+
+    generateId() {
+        const characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let id = '';
+        for (let i = 0; i < 10; i++) {
+            const randomIndex = Math.floor(Math.random() * characters.length);
+            id += characters[randomIndex];
+        }
+        return id;
+    }
 }
 
+// Instancia de product manager
 const productManager = new ProductManager();
 
 console.log(productManager.getProducts());
 
-try {
-    const newProduct = productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-    console.log("Nuevo producto:", newProduct);
-    } catch (error) {
-    console.log("Error al agregar el nuevo producto:", error.message);
-}
+// Agregar un producto
+const productId = productManager.addProduct(
+    'producto prueba',
+    'Este es un producto prueba',
+    200,
+    'Sin imagen',
+    'abc123',
+    25
+);
+console.log('ID del producto agregado:', productId);
 
+// Obtener productos nuevamente
 console.log(productManager.getProducts());
 
-try {
-    productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
-} catch (error) {
-    console.log("Error al agregar el producto:", error.message);
-}
+// Obtener producto por ID
+const productById = productManager.getProductById(productId);
+console.log('Producto por ID:', productById);
 
-try {
-    const product = productManager.getProductById(productManager.getProducts()[0].id);
-    console.log("Producto encontrado por ID:", product);
-} catch (error) {
-    console.log("Error al obtener el producto:", error.message);
-}
+// Actualizar producto
+productManager.updateProduct(productId, 'price', 300);
+console.log('Producto actualizado:', productManager.getProductById(productId));
+
+// Eliminar producto
+productManager.deleteProduct(productId);
+console.log('Productos después de eliminar:', productManager.getProducts());
+
+
+
+
+
+// try {
+//     const newProduct = productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+//     console.log("Nuevo producto:", newProduct);
+//     } catch (error) {
+//     console.log("Error al agregar el nuevo producto:", error.message);
+// }
+
+// console.log(productManager.getProducts());
+
+// try {
+//     productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "Sin imagen", "abc123", 25);
+// } catch (error) {
+//     console.log("Error al agregar el producto:", error.message);
+// }
+
+// try {
+//     const product = productManager.getProductById(productManager.getProducts()[0].id);
+//     console.log("Producto encontrado por ID:", product);
+// } catch (error) {
+//     console.log("Error al obtener el producto:", error.message);
+// }
